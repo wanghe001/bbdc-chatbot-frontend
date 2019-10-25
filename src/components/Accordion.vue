54cc8334d246bf4ead525c2ef5405e57d8bfd071
He Wang <wanghehehehe@gmail.com>
2019-10-25 10:08:03 +0200

    - changed the entity spans to accordions on the background tab - moved the last utterance to a new prediction tab

---
+++ src/components/Accordion.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <h4 class="accordion-title" @click="isVisible=!isVisible">{{title}}</h4>
+      <div v-if="isVisible" class="accordion-body">
+        <div v-html="body"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Accordion',
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+  props: {
+    title: String,
+    body: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .accordion-title{
+    background: rgba(#CC4C58,0.2);
+    margin:5px 5px;
+    padding: 5px;
+    border-radius: 5px;
+    color: #CC4C58;
+    text-align: left;
+  }
+  .accordion-body{
+    color:white;
+
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,32 +18,29 @@
       </div>
     </div>
     <div v-if="loggedIn" class="explanation-window" :class="{expanded: explainTabClicked}">
-      <div class='background'>
+      <div class='background section'>
         <h2 class='section-title'>Background</h2>
-        <span
-          class="entity-name"
-          v-for="(wiki,index) in background" :key="index"
-          @click="setBackgroundKnowledge(wiki)"
-        >{{wiki.rawName}}</span>
+        <accordion v-for="(entity,index) in background"
+                   :key="index"
+                   :title="entity.rawName"
+                   :body="setMessageGradient(entity)"
+        ></accordion>
       </div>
-      <div class='explanation'>
-        <h2>Explanations</h2>
+      <div class='history section'>
+        <h2>History</h2>
         <div class='utterance' v-for="(utterance,index) in explanations" :key='index'>
-          <div v-html="setMessageGradient(utterance)"></div>
+          <div v-if="index<explanations.length-1" v-html="setMessageGradient(utterance)"></div>
         </div>
       </div>
-      <div class="background-modal" v-if='backgroundShown'>
-        <span class='close' @click="backgroundShown=false">X</span>
-        <div class='background-content'>
-          <h3>{{backgroundContent.rawName}}<span class='type'>{{backgroundContent.type}}</span></h3>
-          <div class='background-text'>{{backgroundContent.text}}</div>
-          <div class='domain'>Domains: {{backgroundContent.domains.join(", ")}}</div>
-          <div class="wiki-page">Wiki Page:
-            <a :href="backgroundContent.link" target="_blank">link</a>
+      <div class="prediction section">
+        <h2>Prediction</h2>
+        <div class="utterance">
+          <div>
+            <div v-if="explanations[explanations.length-1]"
+                 v-html="setMessageGradient(explanations[explanations.length-1])"></div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -52,12 +49,14 @@
 // @ is an alias to /src
 import LoginBox from '../components/LoginBox.vue';
 import MessageBox from '../components/MessageBox.vue';
+import Accordion from '../components/Accordion.vue';
 
 export default {
   name: 'home',
   components: {
     LoginBox,
     MessageBox,
+    Accordion,
   },
   computed: {
     loggedIn() {
@@ -85,11 +84,6 @@ export default {
       this.userMessage = '';
       this.$store.dispatch('fetchMessages');
     },
-    setBackgroundKnowledge(bk) {
-      Object.assign(this.backgroundContent, bk);
-      console.log(this.backgroundContent);
-      this.backgroundShown = true;
-    },
     setMessageGradient(message) {
       console.log(message);
       const redRGB = {
@@ -102,22 +96,34 @@ export default {
         green: 76,
         blue: 77,
       };
-      const splittedText = message.utterance.split('');
-      const RGB = message.type === '<user>' ? blackRGB : redRGB;
+      if (message.utterance) {
+        const splittedText = message.utterance.split('');
+        const RGB = message.type === '<user>' ? blackRGB : redRGB;
+        message.annotation.forEach((anno) => {
+          const start = anno[0];
+          const end = anno[0] + anno[1] - 1;
+          const color = `rgba(${RGB.red},${RGB.green},${RGB.blue},${0.2 + anno[2] * 0.8})`;
+          splittedText[start] = `<span class="entity-name" style="background: ${color}">${splittedText[start]}`;
+          splittedText[end] = `${splittedText[end]}</span>`;
+        });
+        const classType = message.type === '<user>' ? 'user' : 'bot';
+        let isLast = false;
+        if (message.annotation.length === 0) {
+          isLast = true;
+        }
+        const lastClass = isLast ? 'last' : '';
+        return `<div class="${classType} ${lastClass}">${splittedText.join('')}</div>`;
+      }
+      const splittedText = message.text.split('');
+      const RGB = redRGB;
       message.annotation.forEach((anno) => {
         const start = anno[0];
         const end = anno[0] + anno[1] - 1;
         const color = `rgba(${RGB.red},${RGB.green},${RGB.blue},${0.2 + anno[2] * 0.8})`;
-        splittedText[start] = `<span class="entity-name" style="background: ${color}">${splittedText[start]}`;
+        splittedText[start] = `<span class="background-token" style="background: ${color}">${splittedText[start]}`;
         splittedText[end] = `${splittedText[end]}</span>`;
       });
-      const classType = message.type === '<user>' ? 'user' : 'bot';
-      let isLast = false;
-      if (message.annotation.length === 0) {
-        isLast = true;
-      }
-      const lastClass = isLast ? 'last' : '';
-      return `<div class="${classType} ${lastClass}">${splittedText.join('')}</div>`;
+      return `<div class="background-text">${splittedText.join('')}</div>`;
     },
   },
 };
@@ -215,43 +221,37 @@ export default {
     z-index: -1;
     transition: 0.4s;
     border-radius: 5px;
+    .section {
+      margin:0 20px;
+      &.background{
+        margin-top:50px;
+      }
+    }
     &.expanded{
       left:50vw;
       transition: 0.4s;
     }
     h2{
-      margin-top:50px;
-      margin-left:20px;
       color: #dd5d69;
       width: 160px;
-      padding: 10px;
-      border-radius: 5px;
+      text-align: left;
     }
     .entity-name {
       background:#CC4C58;
       color:white;
-      padding:5px;
-      margin: 5px;
-      border-radius: 5px;
-      &:hover{
-        background:#BB3B47;
-        cursor: pointer;
-      }
+      padding:2px;
+      margin: 2px;
+      border-radius: 2px;
     }
     .utterance {
-      margin:10px 40px;
+      margin: 10px;
+      text-align: left;
       .context {
         padding: 5px;
         margin:2px;
         border-radius: 5px;
         color: white;
         display: inline-block;
-      }
-      .bot{
-        text-align: left;
-      }
-      .user{
-        text-align: right;
       }
       .last {
         background: #CC4C58;
@@ -272,77 +272,21 @@ export default {
         display: inline-block;
       }
     }
-    .background-modal{
-      position: absolute;
-      z-index: 100;
-      background: rgba($color: #000000, $alpha: 0.5);
-      top:0;
-      left:0;
-      right:0;
-      bottom:0;
-      .close{
-        position:absolute;
-        display: inline-block;
-        right:5px;
-        top: 5px;
-        background: #CC4C58;
-        color:white;
-        width:20px;
-        height: 20px;
-        &:hover{
-          cursor: pointer;
-
-        }
-      }
-      .background-content{
-        background:white;
-        position: absolute;
-        top: 10%;
-        left: 10%;
-        right: 10%;
-        bottom: 10%;
-        h3{
-          color: #dd5d69;
-          padding: 10px;
-          border-radius: 5px;
-          .type {
-            color: white;
-            background: #CC4C58;
-            padding: 5px;
-            border-radius: 20px;
-            font-size:0.5em;
-            display: inline-block;
-            margin:5px;
-
-          }
-        }
-        .background-text{
-          color:#CC4C58;
-          padding:20px;
-          text-align: justify;
-          .context {
-            padding: 5px;
-            margin:2px;
-            border-radius: 5px;
-            color: white;
-            display: inline-block;
-          }
-
-        }
-        .domain{
-          text-align: left;
-          color:#CC4C58;
-          padding-left:20px;
-        }
-        .wiki-page {
-          text-align: left;
-          color:#CC4C58;
-          padding-left:20px;
-          a{
-            color:#CC4C58;
-          }
+    .background {
+      margin-left:20px;
+      .background-text {
+        text-align: justify;
+        margin:0 10px;
+        .background-token {
+          display: inline-block;
+          margin: 2px;
+          padding: 2px;
+          border-radius: 2px;
         }
       }
     }
+  }
+  .background-window {
+
   }
 </style>
